c987f01ef17e1ff289a2ddf9bca7ab8e35cc963b
Axel Schmidt <axel.sebastian.schmidt@rwth-aachen.de>
2020-01-09 13:39:25 +0100

    initial commit to unit test tutorial

--- trial_project/trial_project.py
+++ trial_project/trial_project.py
@@ -9,3 +9,33 @@ def fn1():
 def fn2():
     '''Second function'''
     pass
+
+def add(x,y):
+    """Add Function
+    
+    :param x: x
+    :type x: int, float
+    :param y: y
+    :type y: int, float
+    """
+    return x + y
+
+def subtract(x, y):
+    """Subtract Function
+    
+    :param x: x
+    :type x: int, float
+    :param y: y
+    :type y: int, float
+    """
+    return  x - y
+
+def multiply(x, y):
+    """Multiply Function"""
+    return x * y
+
+def divide(x, y):
+    """Divide Function"""
+    if y ==:
+        raise ValueError('Can not divide by zero!')
+    return x / y
